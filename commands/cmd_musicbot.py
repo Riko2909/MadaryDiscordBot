e9ad8b1a83ffc9a7ce49ae659c00226ac832ef5c
rikorick <riko.gerdes@gmail.com>
2021-05-25 21:00:49 +0200

    MusikBot

--- commands/cmd_musicbot.py
+++ commands/cmd_musicbot.py
@@ -2,20 +2,28 @@ from discord import FFmpegOpusAudio
 from youtube_dl import YoutubeDL
 
 import general
-import discord
-import youtube_dl
 
 client = general.client
-YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist': 'True', 'quiet': 'True'}
+YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist': 'True', 'quiet': 'True', 'ignoreerrors': 'True'}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 queue = {}
 
 
-def getPlayableURl(url):
+def get_info(url):
     with YoutubeDL(YDL_OPTIONS) as ydl:
         info = ydl.extract_info(url, download=False)
-    return info['formats'][0]['url']
+    return info
+
+
+def check_queue(server, voiceclient):
+    if server not in queue:
+        return
+
+    if queue[server]:
+        print(queue[server])
+        voiceclient.play(FFmpegOpusAudio(queue[server].pop(0), **FFMPEG_OPTIONS),
+                         after=lambda x: check_queue(server, voiceclient))
 
 
 @client.command()
@@ -30,25 +38,16 @@ async def join(ctx):
 
 
 @client.command()
-async def stop(ctx):
-    if ctx.author.voice is None:
+async def leave(ctx):
+    if ctx.voice_client is None:
         # Exiting if the user is not in a voice channel
-        return await ctx.send('Du musst in einem Voicechannel sein um diesen Command auszuführen!')
+        return await ctx.send('Der Bot befindet sich nicht in einem Voicechannel!')
 
     await ctx.voice_client.disconnect()
 
 
-def check_queue(server, voiceclient):
-
-    if queue[server]:
-        print(queue[server])
-        voiceclient.play(FFmpegOpusAudio(queue[server].pop(0), **FFMPEG_OPTIONS), after=lambda x: check_queue(server, voiceclient))
-
-
-
 @client.command()
-async def play(ctx, url: str):
-
+async def play(ctx, url: str = None):
     server = ctx.message.guild
 
     if ctx.author.voice is None:
@@ -63,18 +62,23 @@ async def play(ctx, url: str):
     if voiceclient is None:
         voiceclient = await ctx.author.voice.channel.connect()
 
-    URL = getPlayableURl(url)
+    INFO = get_info(url)
 
     if not voiceclient.is_playing():
-        voiceclient.play(FFmpegOpusAudio(URL, **FFMPEG_OPTIONS), after=lambda x: check_queue(server.id, voiceclient))
+        voiceclient.play(
+            FFmpegOpusAudio(INFO['formats'][0]['url'], **FFMPEG_OPTIONS),
+            after=lambda x: check_queue(server.id, voiceclient)
+        )
+        await ctx.send(f"Spiele {INFO['title']} :notes:")
     else:
 
         if server.id in queue:
-            queue[server.id].append(URL)
+            queue[server.id].append([INFO['formats'][0]['url']])
         else:
-            queue[server.id] = [URL]
+            queue[server.id] = [INFO['formats'][0]['url']]
 
-        await ctx.send("Already playing song! Adding to queue")
+        await ctx.send("Es wird bereits ein Song abgespielt! Der Track wird der Warteschlange hinzugefügt :stopwatch: ")
+
 
 @client.command()
 async def next(ctx):
@@ -83,4 +87,35 @@ async def next(ctx):
         return await ctx.send('Du musst in einem Voicechannel sein um diesen Command auszuführen!')
 
     voiceclient = ctx.voice_client
-    voiceclient.stop()
+    server = ctx.message.guild
+
+    if queue[server.id]:
+        voiceclient.stop()
+        voiceclient.play(FFmpegOpusAudio(queue[server.id].pop(0), **FFMPEG_OPTIONS),
+                         after=lambda x: check_queue(server.id, voiceclient))
+    else:
+        await ctx.send('Die Warteschlange ist leer!')
+
+
+@client.command()
+async def pause(ctx):
+    if ctx.author.voice is None:
+        # Exiting if the user is not in a voice channel
+        return await ctx.send('Du musst in einem Voicechannel sein um diesen Command auszuführen!')
+
+    voiceclient = ctx.voice_client
+    if voiceclient.is_playing():
+        voiceclient.pause()
+        await ctx.send('Track wird pausiert!')
+
+
+@client.command()
+async def resume(ctx):
+    if ctx.author.voice is None:
+        # Exiting if the user is not in a voice channel
+        return await ctx.send('Du musst in einem Voicechannel sein um diesen Command auszuführen!')
+
+    voiceclient = ctx.voice_client
+    if voiceclient.is_paused():
+        voiceclient.resume()
+        await ctx.send('Track wird fortgesetzt!')
